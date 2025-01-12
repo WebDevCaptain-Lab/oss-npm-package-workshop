37d050ff86a4ef3b824049f45ec4135970c6ca0b
Shreyash <hello@webdevcaptain.com>
2025-01-12 10:05:37 +0000

    feat(minification): minify the output bundle using terser
    use terser plugin in rollup to minify the generated UMD bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import terser from "@rollup/plugin-terser";
+
 export default {
   input: "src/mathy.js",
   output: {
     name: "mathy",
     file: "dist/bundle.js",
-    format: "umd"
+    format: "umd",
+    plugins: [terser()]
   }
 };
